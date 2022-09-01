1a00255004872022fedd4e134bff124197d48e29
Anorihon <mrsign002@gmail.com>
2022-09-01 21:27:37 +0300

    Update and add print version

--- components/Skill.vue
+++ components/Skill.vue
@@ -1,5 +1,5 @@
 <template>
-  <li>
+  <li class="skill">
     <div class="title">
       {{ title }}
     </div>
@@ -23,52 +23,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.title {
-  font-size: 18px;
-  font-weight: 700;
-  color: #262626;
-  text-transform: uppercase;
-
-  margin-bottom: 5px;
-}
-
-.progress-bar {
-  position: relative;
-  width: 100%;
-  height: 11px;
-  background: #3c3b3a;
-
-  border-top: 1px solid #7f7c7c;
-  border-right: 1px solid #5a5857;
-  border-bottom: 1px solid #e6e5e4;
-  border-left: 1px solid #5a5857;
-
-  > .fill-box {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 0;
-    height: 100%;
-    background: #6a7bba;
-    transition: width .5s;
-
-    border-top: 1px solid #a5add6;
-    border-left: 1px solid #aeb5db;
-
-    > .content {
-      position: absolute;
-      right: 0;
-      bottom: 100%;
-      padding: 5px;
-      background: #6a7bba;
-
-      border-top: 1px solid #a5add6;
-      border-left: 1px solid #aeb5db;
-
-      font-size: 14px;
-      color: #ffffff;
-    }
-  }
-}
-</style>
+<style src="assets/styles/skill.scss" lang="scss"></style>
--- pages/index.vue
+++ pages/index.vue
@@ -12,53 +12,52 @@
       <section id="about-me">
         <h3>About me</h3>
         <div class="text">
-          <p>My main frontend stack is - vanilla JS, Vue/Nuxt, SCSS, Webpack</p>
+          <p>My main frontend stack is - vanilla JS, Vue/Nuxt, Angular, SCSS, Webpack</p>
           <p>Except Photoshop I am familiar with Sketch and Figma</p>
           <p>I have hobby - game development. And that is why i get practice on different languages and tools, like Unity3d (C#), Godot (gdscript looks like Python), Phaser etc</p>
         </div>
-
-        <ul id="contacts">
-          <li class="contact">
-            <div class="title">
-              Phone:
-            </div>
-            <a href="tel:+380677471236">+38 067 747 1236</a>
-          </li>
-          <li class="contact">
-            <div class="title">
-              Email:
-            </div>
-            <a href="mailto:mrsign002@gmail.com">mrsign002@gmail.com</a>
-          </li>
-          <li class="contact">
-            <div class="title">
-              Telegram:
-            </div>
-            <a href="https://t.me/Anorihon">Write me</a>
-          </li>
-          <li class="contact">
-            <div class="title">
-              Website:
-            </div>
-            <a href="https://anorihon.com" target="_blank">anorihon.com</a>
-          </li>
-        </ul>
-
-        <ul id="skills" class="box">
-          <skill-component title="JavaScript" :progress="100" />
-          <skill-component title="Vue" :progress="100" />
-          <skill-component title="Nuxt" :progress="80" />
-          <skill-component title="Angular" :progress="80" />
-          <skill-component title="React" :progress="50" />
-          <skill-component title="TypeScript" :progress="80" />
-          <skill-component title="Phaser" :progress="85" />
-          <skill-component title="Wordpress" :progress="100" />
-          <skill-component title="PHP" :progress="60" />
-          <skill-component title="С#" :progress="60" />
-          <skill-component title="Git" :progress="100" />
-          <skill-component title="SCSS" :progress="100" />
-        </ul>
       </section>
+      <ul id="contacts">
+        <li class="contact">
+          <div class="title">
+            Phone:
+          </div>
+          <a href="tel:+380677471236">+38 067 747 1236</a>
+        </li>
+        <li class="contact">
+          <div class="title">
+            Email:
+          </div>
+          <a href="mailto:mrsign002@gmail.com">mrsign002@gmail.com</a>
+        </li>
+        <li class="contact">
+          <div class="title">
+            Telegram:
+          </div>
+          <a href="https://t.me/Anorihon">@Anorihon</a>
+        </li>
+        <li class="contact">
+          <div class="title">
+            Website:
+          </div>
+          <a href="https://anorihon.com" target="_blank">https://anorihon.com</a>
+        </li>
+      </ul>
+
+      <ul id="skills" class="box">
+        <skill-component title="JavaScript" :progress="100" />
+        <skill-component title="Vue" :progress="100" />
+        <skill-component title="Nuxt" :progress="100" />
+        <skill-component title="Angular" :progress="80" />
+        <skill-component title="React" :progress="50" />
+        <skill-component title="TypeScript" :progress="80" />
+        <skill-component title="Phaser" :progress="100" />
+        <skill-component title="Wordpress" :progress="100" />
+        <skill-component title="PHP" :progress="60" />
+        <skill-component title="С#" :progress="60" />
+        <skill-component title="Git" :progress="100" />
+        <skill-component title="SCSS" :progress="100" />
+      </ul>
     </div>
     <aside class="sidebar">
       <div id="experience" class="box">
@@ -95,7 +94,7 @@
             position="Fullstack developer"
             :start-date="new Date(Date.parse('Jun 2018'))"
             :finish-date="new Date(Date.parse('Sep 2018'))"
-          ></work-place>
+          />
           <work-place
             company="PUMB"
             position="Frontend developer"
